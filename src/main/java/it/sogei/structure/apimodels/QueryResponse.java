9a6cfe3050f4ac1dca8ce4a179d8b982fde9491e
Blaiin <dandyser@hotmail.it>
2024-10-21 16:20:05 +0200

    update 21/10

--- src/main/java/it/sogei/structure/apimodels/QueryResponse.java
+++
@@ -1,9 +0,0 @@
-package it.sogei.structure.apimodels;
-
-import org.eclipse.microprofile.openapi.annotations.media.Schema;
-
-import java.util.List;
-
-@Schema(description = "Model representing a query response")
-public record QueryResponse(String status, String message, List<List<?>> results) {
-}
